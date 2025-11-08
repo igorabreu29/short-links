b83339c081fdf154e49795426385f84391aab920
igorabreu29 <abreusense@gmail.com>
2025-11-08 13:09:06 -0300

    feat: adjusts

--- src/lib/postgres.ts
+++ src/lib/postgres.ts
@@ -1,4 +1,4 @@
-import postgres from "postgres";
-import { env } from "@/env.ts";
+import postgres from 'postgres'
+import { env } from '@/env.ts'
 
-export const sql = postgres(env.DATABASE_URL);
+export const sql = postgres(env.DATABASE_URL)
--- src/setup.ts
+++ src/setup.ts
@@ -1,22 +1,18 @@
-import { sql } from "./lib/postgres.ts";
+import { sql } from './lib/postgres.ts'
 
 async function setup() {
-  await sql`
-    DROP TABLE IF EXISTS short_links;
-  `;
-
-  await sql`
+	await sql`
     CREATE TABLE IF NOT EXISTS short_links (
       id VARCHAR(255) PRIMARY KEY,
       code VARCHAR(255) UNIQUE NOT NULL,
       original_url TEXT,
       created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP
     );
-  `;
+  `
 
-  await sql.end();
+	await sql.end()
 
-  console.log("Setup feito com sucesso!");
+	console.log('Setup feito com sucesso!')
 }
 
-setup();
+setup()
