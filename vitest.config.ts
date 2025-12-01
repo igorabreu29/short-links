4ef8c779883e14e16e95e1963edf0038bac7e473
igorabreu29 <abreusense@gmail.com>
2025-12-01 09:45:57 -0300

    chore: update configs

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,22 +1,21 @@
-import swc from "unplugin-swc";
-import tsConfigPaths from "vite-tsconfig-paths";
-import { defineConfig } from "vitest/config";
+import swc from 'unplugin-swc'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
 	plugins: [
 		tsConfigPaths(),
 		swc.vite({
-			module: { type: "es6" },
+			module: { type: 'es6' },
 		}),
-		swc.rollup(),
 	],
 	test: {
-		setupFiles: ["./tests/setup.ts"],
+		setupFiles: ['./tests/setup.ts'],
 		testTimeout: 60_000,
 		hookTimeout: 60_000,
-		pool: "threads",
+		pool: 'threads',
 		coverage: {
 			enabled: false,
 		},
 	},
-});
+})
